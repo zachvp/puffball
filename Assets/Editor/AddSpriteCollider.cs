a9aade33408a908b7cc1aa875d0870d01cd8720d
zachvp <vegaperk@gmail.com>
2023-08-17 18:18:08 -0600

    poc for custom editor button + monobehavior response

--- Assets/Editor/AddSpriteCollider.cs
+++ Assets/Editor/AddSpriteCollider.cs
@@ -1,7 +1,40 @@
-using System;
+using UnityEditor;
 using UnityEngine;
 
-public class AddSpriteCollider : MonoBehaviour
+[CustomEditor(typeof(ConfigureSpriteCollider))]
+public class AddSpriteCollider : Editor
 {
-    
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Test Button"))
+        {
+            var component = (ConfigureSpriteCollider) target;
+
+            component.Set();
+        }
+    }
 }
+
+
+/*
+ using UnityEditor;
+
+[CustomEditor(typeof(MyCustomComponent))]
+public class MyCustomComponentEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // Draw the default inspector fields
+        DrawDefaultInspector();
+        
+        // Get a reference to the target component
+        MyCustomComponent customComponent = (MyCustomComponent)target;
+
+        // Render a button in the editor
+        if (GUILayout.Button("Press Me"))
+        {
+            customComponent.ButtonPressed();
+        }
+    }
+}
+ */
---
+++ Assets/Scripts/EditorSupport/ConfigureSpriteCollider.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class ConfigureSpriteCollider : MonoBehaviour
+{
+    public void Set()
+    {
+        Debug.Log("fired set");
+    }
+}
